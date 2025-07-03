b99e1e2e85d18fee6534a21530186f7cc5a07e85
nidhi <kumarinidhi0594@gmail.com>
2025-07-03 11:33:46 +0200

    add media query  and fix the layout

--- src/app/Occupancy/OccupancyWidget/OccupancyWidget.component.ts
+++ src/app/Occupancy/OccupancyWidget/OccupancyWidget.component.ts
@@ -1,16 +1,35 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, signal, toSignal, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BarChartComponent } from '../../SharedModule/bar-chart/bar-chart.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+interface OccupancyData {
+  labels: string[];
+  data: number[];
+}
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, BarChartComponent],
+  imports: [CommonModule, BarChartComponent, HttpClientModule],
   templateUrl: './OccupancyWidget.component.html',
   styleUrls: ['./OccupancyWidget.component.css']
 })
 export class DashboardComponent {
   @Output() delete = new EventEmitter<void>();
+  private http = inject(HttpClient);
+
+  occupancyData = toSignal(
+    this.http.get<OccupancyData>('assets/mock-occupancy-data.json'),
+    { initialValue: { labels: [], data: [] } }
+  );
+
+  get labels() {
+    return this.occupancyData().labels;
+  }
+  get data() {
+    return this.occupancyData().data;
+  }
 
   onDelete() {
     this.delete.emit();
@@ -18,4 +37,3 @@ export class DashboardComponent {
 }
 
 
-  
