30a349937da525fe28ad1a77839a77739bb35ad5
nidhi <kumarinidhi0594@gmail.com>
2025-07-03 12:49:15 +0200

    Occupancy-Mockdata

--- src/app/Occupancy/OccupancyWidget/OccupancyWidget.component.ts
+++ src/app/Occupancy/OccupancyWidget/OccupancyWidget.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output, inject } from '@angular/core';
+import { Component, EventEmitter, Output, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BarChartComponent } from '../../SharedModule/bar-chart/bar-chart.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
@@ -15,9 +15,19 @@ interface OccupancyData {
   templateUrl: './OccupancyWidget.component.html',
   styleUrls: ['./OccupancyWidget.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   @Output() delete = new EventEmitter<void>();
   private http = inject(HttpClient);
+
+  labels: string[] = [];
+  data: number[] = [];
+
+  ngOnInit() {
+    this.http.get<OccupancyData>('mock-data.json').subscribe(res => {
+      this.labels = res.labels;
+      this.data = res.data;
+    });
+  }
 
   onDelete() {
     this.delete.emit();
